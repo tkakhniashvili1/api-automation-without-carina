1459d2a03e6ef622b0c841ce9eccfaed3f4343ea
Tina Kakhniashvili <tkakhniashvili@solvs.com>
2026-03-23 16:46:39 +0400

    Add initial project setup files

---
+++ src/test/java/com/solvd/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.solvd;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+    @Test
+    public void smokeTest() {
+        Assert.assertTrue(true);
+    }
+}
